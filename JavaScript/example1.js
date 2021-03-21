de1f9d12a5c2db0d6984fcbbc36588001e86cecc
Min <decemberkms@gmail.com>
2021-03-21 20:52:48 +0100

    Add js 1

---
+++ JavaScript/example1.js
@@ -0,0 +1,7 @@
+alert("Hi!")
+var deposit = prompt ("How much?")
+alert("You've deposited: " + deposit)
+console.log("Your saving: " + deposit)
+for (var i = 0; i < 5; i++) {
+  console.log("Number is " +i);
+}
